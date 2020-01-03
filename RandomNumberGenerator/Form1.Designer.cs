e2ed4c14d7c4e293f1ded20fc292739c3ffb6e8e
alex-code4okc <jayonloza@gmail.com>
2020-01-03 09:23:25 -0600

    First commit

---
+++ RandomNumberGenerator/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace RandomNumberGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.GenerateBtn = new System.Windows.Forms.Button();
+            this.RandomTxtBox = new System.Windows.Forms.TextBox();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Generate a random 4 digit number.";
+            
+            // 
+            // GenerateBtn
+            // 
+            this.GenerateBtn.Location = new System.Drawing.Point(344, 42);
+            this.GenerateBtn.Name = "GenerateBtn";
+            this.GenerateBtn.Size = new System.Drawing.Size(83, 23);
+            this.GenerateBtn.TabIndex = 1;
+            this.GenerateBtn.Text = "Generate";
+            this.GenerateBtn.UseVisualStyleBackColor = true;
+            this.GenerateBtn.Click += new System.EventHandler(this.GenerateBtn_Click);
+            // 
+            // RandomTxtBox
+            // 
+            this.RandomTxtBox.Location = new System.Drawing.Point(72, 75);
+            this.RandomTxtBox.Name = "RandomTxtBox";
+            this.RandomTxtBox.Size = new System.Drawing.Size(227, 22);
+            this.RandomTxtBox.TabIndex = 2;
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Location = new System.Drawing.Point(344, 73);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(83, 23);
+            this.ClearBtn.TabIndex = 3;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.RandomTxtBox);
+            this.Controls.Add(this.GenerateBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button GenerateBtn;
+        private System.Windows.Forms.TextBox RandomTxtBox;
+        private System.Windows.Forms.Button ClearBtn;
+    }
+}
+
---
+++ RandomNumberGenerator/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumberGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int GenerateRandomDigits()
+        {
+            Random random = new Random();
+            int random_digits = random.Next(1000, 10000);
+            return random_digits;
+        }
+
+        private void GenerateBtn_Click(object sender, EventArgs e)
+        {
+            int result = GenerateRandomDigits();
+            RandomTxtBox.Text = result.ToString();
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            RandomTxtBox.Text = "";
+        }
+    }
+}
